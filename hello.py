ccc7581a22a31c41940b47de5978fd362bf59001
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2023-05-03 21:24:52 +0545

    capitalize

--- hello.py
+++ hello.py
@@ -4,5 +4,8 @@ name = input("What's your name? ")
 # remove whitespace from the string
 name = name.strip()
 
+# capitalize the name
+name = name.capitalize()
+
 # say hello to user
 print(f"Hello, {name}")
