11deb15e90858cfc218d98715073493ad6c030f8
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2023-05-09 20:05:37 +0545

    c
    lecture 1

--- calculator.py
+++
@@ -1,6 +0,0 @@
-x = float(input("what is x?"))
-y = float(input("what is y?"))
-
-z = x/y
-
-print(f"{z:.2f}")
--- hello.py
+++
@@ -1,6 +0,0 @@
-def hello(to):
-    print("hello,", to)
-
-
-name = input("what's your name? ")
-hello(name)
