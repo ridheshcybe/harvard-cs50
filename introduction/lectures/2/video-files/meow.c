679af8a33bee9e238f5cc998eb55ddf09876fa35
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2023-05-31 17:05:21 +0545

    Update meow.c
    1:27:51

--- introduction/lectures/2/video-files/meow.c
+++ introduction/lectures/2/video-files/meow.c
@@ -2,7 +2,10 @@
 
 int main(void)
 {
-    printf("meow\n");
-    printf("meow\n");
-    printf("meow\n");
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
 }
